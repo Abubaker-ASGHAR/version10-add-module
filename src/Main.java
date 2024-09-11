2d655238f317ae512f0919260621fcff2b3f0fc5
abubaker <abubaker.asghar@techno-soft.com>
2024-09-11 16:00:27 +0500

    second commit including module file

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 5;
+        int square = Util .square(number);
+        System.out.println("The square of " + number + " is: " + square);
+    }
+}
---
+++ version101module/src/Util.java
@@ -0,0 +1,5 @@
+public class Util {
+    public static int square(int num) {
+        return num * num;
+    }
+}
